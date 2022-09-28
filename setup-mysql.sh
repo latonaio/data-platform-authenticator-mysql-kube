f7f49729efefb007989c0bea6c787580fa7f69d3
Yurino Oga <yurino.o@latona.io>
2022-09-29 00:58:11 +0900

    release

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-DB_USER_NAME="root" # DBのユーザー名を指定
-DB_USER_PASSWORD="root" # DBのパスワードを指定
+DB_ROOT_NAME="XXXXXXXX" # DBのルートユーザーを指定
+DB_ROOT_PASSWORD="XXXXXXXX" # DBのルートパスワードを指定
+DB_USER_NAME="XXXXXXXX" # DBのユーザー名を指定
+DB_USER_PASSWORD="XXXXXXXX" # DBのパスワードを指定
 POD_NAME="data-platform-authenticator-mysql-kube" # Podの名前を指定
-DATABASE_NAME="DataPlatformAuthenticatorMySQLKube" # データベース名を指定
-SQL_FILE="data-platform-authenticator-sql-business-user-data.sql" # sqlファイルの名前を指定
+DATABASE_NAME="DataPlatformAuthenticatorSQL" # データベース名を指定
+SQL_FILES=( # sqlファイルの名前を複数指定
+  "data-platform-authenticator-sql-business-user-data.sql"
+  )
 MYSQL_POD=$(kubectl get pod | grep ${POD_NAME} | awk '{print $1}')
 
-kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_USER_NAME} -p${DB_USER_PASSWORD} -e \"CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME} default character set utf8 ;\""
+kubectl exec -it ${MYSQL_POD} -- bash -c "mkdir -p /src"
 
-kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_USER_NAME} -p${DB_USER_PASSWORD} -D ${DATABASE_NAME} < /src/${SQL_FILE}"
+# sqlファイルのコピー
+for ((i=0; i<${#SQL_FILES[@]}; i++)) do
+  kubectl cp ${SQL_FILES[i]} ${MYSQL_POD}:/src
+done
+
+# databaseの作成
+kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_ROOT_NAME} -p${DB_ROOT_PASSWORD} -e \"CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME} default character set utf8;\""
+
+# userに権限の付与
+kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_ROOT_NAME} -p${DB_ROOT_NAME} -e \"GRANT ALL ON *.* TO ${DB_USER_NAME}@;\""
+
+# kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_ROOT_NAME} -p${DB_ROOT_PASSWORD} -e \"USE ${DATABASE_NAME};\""
+
+# テーブルの作成
+# for ((i=0; i<${#SQL_FILES[@]}; i++)) do
+#   kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_USER_NAME} -p${DB_USER_PASSWORD} -D ${DATABASE_NAME} < /src/${SQL_FILES[i]}"
+# done
